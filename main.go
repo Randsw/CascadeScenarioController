ae9c30dea84667cfb512f5062e478d0d584d362a
Randsw <randsw@mail.ru>
2022-07-08 13:31:22 +0300

    Move k8sclient in separate package

--- main.go
+++ main.go
@@ -1,137 +1,21 @@
 package main
 
 import (
-	"context"
 	"os"
-	"strconv"
 	"strings"
-
 	scenarioconfig "github.com/randsw/cascadescenariocontroller/cascadescenario"
+	k8sClient "github.com/randsw/cascadescenariocontroller/k8sclient"
 	"github.com/randsw/cascadescenariocontroller/logger"
 	webhook "github.com/randsw/cascadescenariocontroller/webhook"
 	"go.uber.org/zap"
-	batchv1 "k8s.io/api/batch/v1"
-	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	kubernetes "k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	clientcmd "k8s.io/client-go/tools/clientcmd"
 )
 
-type JobStatus int
-
 const (
-	notStarted JobStatus = iota
+	notStarted k8sClient.JobStatus = iota
 	Running
 	Succeeded
 	Failed
 )
-
-type s3PackagePath struct {
-	stageNum    int
-	path        string
-	isLastStage bool
-}
-
-func connectToK8s() *kubernetes.Clientset {
-	var kubeconfig string
-
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		// fallback to kubeconfigkubeconfig := filepath.Join("~", ".kube", "config")
-		if envvar := os.Getenv("KUBECONFIG"); len(envvar) > 0 {
-			kubeconfig = envvar
-		}
-		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
-		if err != nil {
-			logger.Zaplog.Error("The kubeconfig cannot be loaded", zap.String("err", err.Error()))
-			os.Exit(1)
-		}
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		logger.Zaplog.Error("Failed to create K8s clientset")
-		os.Exit(1)
-	}
-
-	return clientset
-}
-
-func launchK8sJob(clientset *kubernetes.Clientset, namespace string, config *scenarioconfig.CascadeScenarios, s3path *s3PackagePath) {
-	jobs := clientset.BatchV1().Jobs(namespace)
-
-	labels := map[string]string{"app": "Cascade", "modulename": config.ModuleName}
-	// Get Job pod spec
-	JobTemplate := config.Template
-	// Get Scenario parameters
-	ScenarioParameters := config.Configuration
-
-	var podEnv []v1.EnvVar
-	// Fill pod env vars with scenario parameters
-	for key, value := range ScenarioParameters {
-		podEnv = append(podEnv, v1.EnvVar{Name: key, Value: value})
-	}
-	if s3path.stageNum > 0 {
-		split := strings.Split(s3path.path, ".tgz")
-		podEnv = append(podEnv, v1.EnvVar{Name: "s3path", Value: split[0] + "-stage-" + strconv.Itoa(s3path.stageNum) + ".tgz"})
-	}
-	if s3path.isLastStage {
-		podEnv = append(podEnv, v1.EnvVar{Name: "finalstage", Value: "true"})
-	}
-	JobTemplate.Spec.Containers[0].Env = podEnv
-
-	jobSpec := &batchv1.Job{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      config.ModuleName,
-			Namespace: namespace,
-			Labels:    labels,
-		},
-		Spec: batchv1.JobSpec{
-			Template:                JobTemplate,
-			TTLSecondsAfterFinished: config.TTLSecondsAfterFinished,
-			BackoffLimit:            config.BackoffLimit,
-			ActiveDeadlineSeconds:   config.ActiveDeadlineSeconds,
-		},
-	}
-
-	_, err := jobs.Create(context.TODO(), jobSpec, metav1.CreateOptions{})
-	if err != nil {
-		logger.Zaplog.Fatal("Failed to create K8s job.", zap.String("error", err.Error()))
-	}
-
-	//print job details
-	logger.Zaplog.Info("Created K8s job successfully", zap.String("JobName", config.ModuleName))
-}
-
-func getJobStatus(clientset *kubernetes.Clientset, jobName string, jobNamespace string) (JobStatus, error) {
-	job, err := clientset.BatchV1().Jobs(jobNamespace).Get(context.TODO(), jobName, metav1.GetOptions{})
-	if err != nil {
-		return notStarted, err
-	}
-
-	if job.Status.Active == 0 && job.Status.Succeeded == 0 && job.Status.Failed == 0 {
-		return notStarted, nil
-	}
-
-	if job.Status.Succeeded > 0 {
-		logger.Zaplog.Info("Job ran successfully", zap.String("JobName", jobName))
-		return Succeeded, nil // Job ran successfully
-	}
-
-	if job.Status.Failed > 0 {
-		logger.Zaplog.Error("Job ran failed", zap.String("JobName", jobName))
-		return Failed, nil // Job ran successfully
-	}
-
-	return Running, nil
-}
-
-func deleteSuccessJob(clientset *kubernetes.Clientset, jobName string, jobNamespace string) error {
-	background := metav1.DeletePropagationBackground
-	err := clientset.BatchV1().Jobs(jobNamespace).Delete(context.TODO(), jobName, metav1.DeleteOptions{PropagationPolicy: &background})
-	return err
-}
 
 func main() {
 	//Loger Initialization
@@ -140,48 +24,44 @@ func main() {
 	//Get Config from file mounted in tmp folder
 	configFilename := "/tmp/configuration"
 
-	//configFilename := "example_cm.yaml"
+	//configFilename := "cascadescenario/test/test_fail_first.json"
 
 	CascadeScenatioConfig := scenarioconfig.ReadConfigJSON(configFilename)
-
-	//Connect to k8s api server
-	k8sAPIClientset := connectToK8s()
-
 	//Get pod namespace
-	jobNamespace := "default"
+	jobNamespace := "cascade-manual"
 	if envvar := os.Getenv("POD_NAMESPACE"); len(envvar) > 0 {
 		jobNamespace = envvar
 	}
-
 	//Get status server address
-	statusServerAddress := "127.0.0.1:8000"
+	statusServerAddress := "http://127.0.0.1:8000"
 	if envvar := os.Getenv("STATUS_SERVER"); len(envvar) > 0 {
 		statusServerAddress = envvar
 	}
-
-	//Get status server address
+	//Get scenario name
 	scenarioName := "Test-image-processing"
 	if envvar := os.Getenv("SCENARIO_NAME"); len(envvar) > 0 {
 		scenarioName = envvar
 	}
+	//Connect to k8s api server
+	k8sAPIClientset := k8sClient.ConnectToK8s()
 
 	// Initialize s3path struct
-	s3PackagePath := new(s3PackagePath)
+	s3PkgPath := new(k8sClient.S3PackagePath)
 	for i, jobConfig := range CascadeScenatioConfig {
 		// First stage. Get path from config
 		if i == 0 {
-			s3PackagePath.path = jobConfig.Configuration["s3path"]
+			s3PkgPath.Path = jobConfig.Configuration["s3path"]
 		}
 		if i == len(CascadeScenatioConfig)-1 {
-			s3PackagePath.isLastStage = true
+			s3PkgPath.IsLastStage = true
 		}
-		s3PackagePath.stageNum = i
+		s3PkgPath.StageNum = i
 		//Start k8s job
-		launchK8sJob(k8sAPIClientset, jobNamespace, &jobConfig, s3PackagePath)
+		k8sClient.LaunchK8sJob(k8sAPIClientset, jobNamespace, &jobConfig, s3PkgPath)
 		start := true
 		//Check for job status
 		for {
-			status, err := getJobStatus(k8sAPIClientset, jobConfig.ModuleName, jobNamespace)
+			status, err := k8sClient.GetJobStatus(k8sAPIClientset, jobConfig.ModuleName, jobNamespace)
 			if err != nil {
 				logger.Zaplog.Error("Get Job status fail", zap.String("JobName", jobConfig.ModuleName), zap.String("error", err.Error()))
 			}
@@ -198,7 +78,7 @@ func main() {
 					logger.Zaplog.Error("Webhook return fail code", zap.String("error", err.Error()))
 				}
 				// Delete finished Job
-				err = deleteSuccessJob(k8sAPIClientset, jobConfig.ModuleName, jobNamespace)
+				err = k8sClient.DeleteSuccessJob(k8sAPIClientset, jobConfig.ModuleName, jobNamespace)
 				if err != nil {
 					logger.Zaplog.Error("Failed to delete successfull job", zap.String("JobName", jobConfig.ModuleName), zap.String("error", err.Error()))
 				}
@@ -217,7 +97,7 @@ func main() {
 		}
 	}
 	logger.Zaplog.Info("Scenario execution finished successfully")
-	split := strings.Split(s3PackagePath.path, ".tgz")
+	split := strings.Split(s3PkgPath.Path, ".tgz")
 	resultS3path := split[0] + "-final" + ".tgz"
 	statusCode, err := webhook.SendWebHook("Scenario "+scenarioName+" completed successfully. Package address - "+resultS3path, statusServerAddress)
 	if err != nil {
